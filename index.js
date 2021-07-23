708dc92b51438292158239857fd627b96c5dc653
katzumar <84904345+katzumar@users.noreply.github.com>
2021-07-23 03:20:42 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,19 +1,34 @@
 var scene, camera, renderer;
 
 scene = new THREE.Scene();
-scene.background = new THREE.Color(0xff5f0f)
 
-camera = new THREE.PerspectiveCamera(50, window.innerWidth/window.innerHeight);
-camera.position.set(0, 100, 800);
+camera = new THREE.PerspectiveCamera(
+    75, 
+    window.innerWidth/window.innerHeight,
+    0.5,
+    1000);
+camera.position.z = 5;
+
 
 renderer = new THREE.WebGL1Renderer();
 renderer.setSize(window.innerWidth,window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
 
+//cube
+var geometry = new THREE.BoxGeometry(1,1,1);
+var materal = new THREE.meshBasicMaterial({color: 0x00aaff});
+
+var cube = new THREE.Mesh(geometry, materal);
+scene.add(cube);
+
 
 function animate(){
-    requestAnimationFrame(animate)
-    renderer.render(scene,camera)
+    requestAnimationFrame(animate);
+
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+
+    renderer.render(scene,camera);
 }
 animate()
